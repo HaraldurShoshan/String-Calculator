e629b7d792abada9303ff9ca0607d8a23d321a64
HaraldurShoshan <haraldurshoshan@gmail.com>
2016-10-11 16:23:27 +0000

    Added testcase for negative numbers exception, implemented the Negative exception in Calculator.java

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,10 +1,28 @@
 package is.ru.stringcalculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Calculator {
 
 	private static String splitter = ",|\\n";
+	
 
 	public static int add(String text){
+		ArrayList<Integer> negativeNumbers = new ArrayList<Integer>();
+		
+		if(!text.equals("")){
+			String[] myNumbers  = splitNumbers(text);
+			addToArray(myNumbers, negativeNumbers);
+		}
+		
+
+		if(negativeNumbers.size() > 0){
+			String negNumbers = createNegativeString(negativeNumbers);
+
+			throw new IllegalArgumentException("Negatives not allowed: " + negNumbers);
+		}
+	
 		if(text.equals("")){
 			return 0;
 		}else if(text.contains(",") || text.contains("\n")){
@@ -22,6 +40,34 @@ public class Calculator {
 	private static String[] splitNumbers(String numbers){
 
 		return numbers.split(splitter);
+	}
+
+	private static void addToArray(String[] numbers, ArrayList<Integer> negativeNumbers){
+		for(String number : numbers){
+			int x = toInt(number);			
+			if(x < 0){
+				negativeNumbers.add(x);
+			}
+			
+		}
+	}
+
+	private static String createNegativeString(ArrayList<Integer> negativeNumbers){
+		String finalString = "";
+		int count = 0;
+
+		for(Integer num : negativeNumbers){
+			String x = Integer.toString(num);
+
+			if(count == 0){
+				finalString = x;
+				count++;
+			}else{
+				finalString = finalString + "," + x;
+			}
+		}
+
+		return finalString;
 	}
 
 	private static int sum(String[] numbers){
--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -3,12 +3,20 @@ package is.ru.stringcalculator;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.hamcrest.Matcher;
+
 public class CalculatorTest {
 
 	public static void main(String args[]) {
       org.junit.runner.JUnitCore.main("is.ru.stringcalculator.CalculatorTest");
     }
 
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+   
     @Test
     public void testEmptyString(){
     	assertEquals(0, Calculator.add(""));
@@ -37,6 +45,13 @@ public class CalculatorTest {
     @Test
     public void testNewLine(){
     	assertEquals(6, Calculator.add("1\n2,3"));
+    }
+
+    @Test
+    public void testNegative(){
+    	exception.expect(IllegalArgumentException.class);
+    	exception.expectMessage("Negatives not allowed: -1,-2");
+    	Calculator.add("-1,-2,3");
     }
 
 }
